3ad4e0b67c6d6904b0c5ed264bd5a4a5d6e90a91
Diegorodri14 <diegorodriguez14@gmail.com>
2025-06-05 15:43:55 -0600

    Funcion para agregar casi finalizada

--- JS/script.js
+++ JS/script.js
@@ -42,3 +42,43 @@ function MostrarDatos(datos){
 }
 
 ObtenerIntegrates();
+
+
+
+
+//Procesos para agregar un nuevo integrante
+const modal = document.getElementById("mdAgregar"); //Cuadro de dialogo
+const btnAgregar = document.getElementById("btnAgregar"); //Boton para agregar registro
+const btnCerrar = document.getElementById("btnCerrar"); //Boton paraa cerrar el popup
+
+btnAgregar.addEventListener("click", ()=>{
+    modal.showModal(); //Abrir el modal al hcer click boton
+});
+
+btnCerrar.addEventListener("click", ()=>{
+    modal.close(); //Cerrar el modal al hacer click
+});
+
+//Agregar nuevo integrante desde el formulario
+document.getElementById("frmAgregar").addEventListener("submit", async e => {
+    e.preventDefault(); //"e" representa a "submit". Evita que el formulario se envíe de un solo.
+
+    //Capturar los valores del formulario
+    const Nombre = document.getElementById("txtNombre").value.trim();
+    const Apellido = document.getAnimations("txtApellido").value.trim();
+    const Correo = document.getElementById("txtCorreo").value.trim();
+
+    //Validacion basica
+    if(!Nombre || !Apellido || !Correo){
+        alert("Ingrese los valores correctamente");
+        return; //Para evitar que el codigo se siga ejecutando
+    }
+
+    //Llamar a la API para enviar el registro
+    const respuesta = await fetch(API_URL, {
+        method: "POST"
+    });
+
+
+});
+
